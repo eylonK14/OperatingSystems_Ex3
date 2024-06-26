962cebc0f9cefe105549e70e4867cadfeec7ef68
eylonK14 <eylony1403@gmail.com>
2024-06-26 23:04:55 +0300

    removed kosaraju.h

--- kosaraju.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void send_to_everyone(int fd_count, struct pollfd *pfds, int listener, int sender_fd, char *result, int nbytes);
-
-void parseCommand(char *input, char **command, int word_count);
